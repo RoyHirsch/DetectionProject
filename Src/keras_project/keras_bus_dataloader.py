c01f6794f4e71a70b56f21e2dc616c3b102d24fe
RoyHirsch <roymhirsch@gmail.com>
2018-12-31 14:56:17 +0200

    Updated keras_buf_dataloader

--- Src/keras_project/keras_bus_dataloader.py
+++ Src/keras_project/keras_bus_dataloader.py
@@ -5,6 +5,7 @@ import pandas as pd
 import cv2 as cv2
 import os
 import keras
+import matplotlib.pyplot as plt
 
 class DataGenerator(keras.utils.Sequence):
 
@@ -63,12 +64,15 @@ class DataGenerator(keras.utils.Sequence):
 
                 scale_rois = []
                 for roi in rois:
-                    scale_roi = [int(roi[0] * self.scale_h), int(roi[1] * self.scale_w),
-                                 int(roi[1] * self.scale_h), int(roi[3] * self.scale_w)]
+                    # Original roi is in form : [xmin, ymin, width, height, class]
+                    # transform to [xmin, ymin, xmax, ymax, class] -> [xmin, ymin, xmin + width, ymin + height, class]
+                    roi_points = [roi[0], roi[1], roi[0] + roi[2], roi[1] + roi[3], roi[4]]
 
-                    scale_roi.append(roi[-1])
-                    scale_roi = self.conv_rect_to_opos_points(scale_roi)
-                    scale_rois.append(scale_roi)
+                    # Scale the coordinates to the resized image
+                    roi_scaled = [int(roi_points[0] * self.scale_w), int(roi_points[1] * self.scale_h),
+                                  int(roi_points[2] * self.scale_w), int(roi_points[3] * self.scale_h), roi_points[-1]]
+
+                    scale_rois.append(roi_scaled)
 
                 # Good np format for the augmentation package
                 np_rois = np.array(scale_rois)[:, :-1]
@@ -106,20 +110,21 @@ class DataGenerator(keras.utils.Sequence):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
 
-        X = np.empty((self.batch_size, self.dim, self.dim, self.n_channels))
+        X = np.zeros([self.batch_size, self.dim, self.dim, self.n_channels]).astype(np.uint8)
         y = []
 
-        # Generate data
+        # Generate data batch
         for num, ind in enumerate(indexes):
             # Store sample
-            im_name, X[num,] = self.gt_images.loc[ind]
-            im_name, augmented_rects, gt_rects = self.gt_labels.loc[self.gt_labels['image_name'] == im_name + '.JPG'].values[0]
-            y.append(augmented_rects)
+            im_name, X[num, :, :, :] = self.gt_images.loc[ind]
+            im_name, bbox_only, bbxox_n_class = self.gt_labels.loc[self.gt_labels['image_name'] == im_name + '.JPG'].values[0]
+            y.append(bbox_only)
 
         return X, y
 
+
 root_data_dir = '/Users/royhirsch/Documents/Study/Current/ComputerVision/project/busesTrain'
-label_path = '/Users/royhirsch/Documents/GitHub/DetectionProject/annotationsTrain.txt'
+label_path = '/Users/royhirsch/Documents/Study/Current/ComputerVision/project/annotationsTrain.txt'
 
 dg = DataGenerator(root_data_dir, label_path)
 dg.__getitem__(1)
